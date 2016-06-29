fe71bc482e159e62adc31adbed64ac7bf5843c23
Alejandro Duarte <alejandro@vaadin.com>
2016-06-29 15:16:08 +0300

    Added filter

--- src/main/java/com/example/CityMapper.java
+++ src/main/java/com/example/CityMapper.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -10,5 +11,8 @@ public interface CityMapper {
 
     @Select("SELECT * FROM COMPANY")
     List<Company> findAll();
+
+    @Select("SELECT * FROM COMPANY WHERE LOWER(name) LIKE CONCAT('%', LOWER(#{name}), '%')")
+    List<Company> findByName(@Param("name") String name);
 
 }
--- src/main/java/com/example/VaadinUI.java
+++ src/main/java/com/example/VaadinUI.java
@@ -5,7 +5,9 @@ import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -19,8 +21,19 @@ public class VaadinUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        List<Company> companies = cityMapper.findAll();
-        Grid grid = new Grid(new BeanItemContainer<Company>(Company.class, companies));
-        setContent(grid);
+        List<Company> allCompanies = cityMapper.findAll();
+        Grid grid = new Grid(new BeanItemContainer<>(Company.class, allCompanies));
+
+        TextField filter = new TextField();
+        filter.setInputPrompt("filter by name...");
+        filter.addTextChangeListener(e -> {
+            List<Company> companies = cityMapper.findByName(e.getText());
+            grid.setContainerDataSource(new BeanItemContainer<>(Company.class, companies));
+        });
+
+        VerticalLayout layout = new VerticalLayout(filter, grid);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        setContent(layout);
     }
 }
